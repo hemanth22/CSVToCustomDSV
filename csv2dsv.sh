70d3f8be8b2b2d2b1bba457f32c55e33c202d330
Hemanth B <hemanth22hemu@gmail.com>
2018-01-22 06:51:10 +0000

    Adding listing command to script
    Adding listing command to script

--- csv2dsv.sh
+++ csv2dsv.sh
@@ -2,3 +2,7 @@ echo "GetEnvironment"
 cat /etc/*release
 echo "Executing main command"
 cat ESS.csv | column -t -s '"' -o '' | column -t -s ',' -o ';' > ESS.dsv
+echo "Execution complete"
+echo "Listing execution result"
+ls -la
+echo "Finished Listing"
