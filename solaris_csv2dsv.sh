092ccb08253385fa02b3fb3cd2d3676629bbe571
hemanth22 <hemanth22hemu@gmail.com>
2018-02-09 11:54:55 +0530

    Solaris Script

---
+++ solaris_csv2dsv.sh
@@ -0,0 +1,8 @@
+echo "GetEnvironment"
+cat /etc/*release
+echo "Executing main command"
+cat ESS.csv | column -t -s '"' -o '' | column -t -s ',' -o ';' > ESS.dsv
+echo "Execution complete"
+echo "Listing execution result"
+ls -la
+echo "Finished Listing"
