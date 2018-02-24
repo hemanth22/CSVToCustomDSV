1663992cff0b0975995a0a081345e70216629c3b
Hemanth B <hemanth22hemu@gmail.com>
2018-02-24 05:19:32 +0000

    Update csv2dsv.sh
    replace o with c

--- csv2dsv.sh
+++ csv2dsv.sh
@@ -1,7 +1,7 @@
 echo "GetEnvironment"
 cat /etc/*release
 echo "Executing main command"
-cat ESS.csv | column -t -s '"' -o '' | column -t -s ',' -o ';' > ESS.dsv
+cat ESS.csv | column -t -s '"' -c '' | column -t -s ',' -c ';' > ESS.dsv
 echo "Execution complete"
 echo "Listing execution result"
 ls -la
