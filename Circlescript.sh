6d33b91023bc86ccd49803a0faa3d19708c73a41
hemanth22 <hemanth22hemu@gmail.com>
2018-05-02 11:30:40 +0530

    Adding addition scripts for testing

--- Circlescript.sh
+++ Circlescript.sh
@@ -1,5 +1,15 @@
+docker echo "Install Dependency apps."
+echo "Installing EPEL repository."
 yum -y install epel-release
+echo "EPEL Installation complete."
+echo "Install git command."
 yum -y install git
+echo "Git command installation completed."
+echo "Clone My repository."
 git clone https://github.com/hemanth22/CSVToCustomDSV.git
+echo "Cloning repository completed."
+echo "Checking repository."
 cd CSVToCustomDSV
+echo "Running Tests."
 ./csv2dsv.sh"
+echo "Test Completed."
